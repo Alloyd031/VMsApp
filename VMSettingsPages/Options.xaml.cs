2e3c60347e6d69e87fd55c8c53a22db23ccd51b3
Alloyd <kirillpravikov@outlook.com>
2023-11-11 18:14:04 +0300

    Even more

--- VMSettingsPages/Options.xaml.cs
+++ VMSettingsPages/Options.xaml.cs
@@ -14,14 +14,8 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace VMsApp.VMSettingsPages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class Options : Page
     {
         public Options()
@@ -46,7 +40,7 @@ namespace VMsApp.VMSettingsPages
         {
             this.OptionsFrame.Navigate(typeof(NotAvailable));
         }
-        private void UWPToolsButton_Click(object sender, RoutedEventArgs e)
+        private void VMsToolsButton_Click(object sender, RoutedEventArgs e)
         {
             this.OptionsFrame.Navigate(typeof(NotAvailable));
         }
