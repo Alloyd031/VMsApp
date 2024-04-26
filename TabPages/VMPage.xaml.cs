bfb22526dbc9fd3ba596f5d508cf80e23a2fb12a
Alloyd <Alloyd@HOME-PC>
2024-04-26 20:10:50 +0300

    Forgot something

--- TabPages/VMPage.xaml.cs
+++ TabPages/VMPage.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -9,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -23,10 +25,45 @@ namespace VMsApp.TabPages
         {
             this.InitializeComponent();
         }
+        public static void CreateModalWindow(WindowEx parentWindow, WindowEx childWindow, bool summonWindowAutomatically = true, bool blockInput = false)
+        {
+            IntPtr hWndChildWindow = WinRT.Interop.WindowNative.GetWindowHandle(childWindow);
+            IntPtr hWndParentWindow = WinRT.Interop.WindowNative.GetWindowHandle(parentWindow);
+            SetWindowLong(hWndChildWindow, GWL_HWNDPARENT, hWndParentWindow);
+            (childWindow.AppWindow.Presenter as OverlappedPresenter).IsModal = true;
+            if (blockInput == true)
+            {
+                EnableWindow(hWndParentWindow, false);
+                childWindow.Closed += ChildWindow_Closed;
+                void ChildWindow_Closed(object sender, Microsoft.UI.Xaml.WindowEventArgs args)
+                {
+                    EnableWindow(hWndParentWindow, true);
+                }
+            }
+            if (summonWindowAutomatically == true) childWindow.Show();
+        }
+        private static IntPtr SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong)
+        {
+            if (IntPtr.Size == 4)
+            {
+                return SetWindowLongPtr32(hWnd, nIndex, dwNewLong);
+            }
+            return SetWindowLongPtr64(hWnd, nIndex, dwNewLong);
+        }
+        [DllImport("User32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        private static extern bool EnableWindow(IntPtr hWnd, bool bEnable);
+
+        private const int GWL_HWNDPARENT = (-8);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto, EntryPoint = "SetWindowLong")]
+        private static extern IntPtr SetWindowLongPtr32(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto, EntryPoint = "SetWindowLongPtr")]
+        private static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
         private void EditVMSettings_Click(object sender, RoutedEventArgs e)
         {
-            m_window = new VMSettings();
-            m_window.Show();
+            var logWin = new VMSettings();
+            CreateModalWindow(App.m_window, logWin, true, true);
         }
     }
 }
