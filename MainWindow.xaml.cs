609d166a4cabc73b03c372117f947da0a0a929cb
jpb <jakub.baranski.33@gmail.com>
2024-06-02 17:22:52 +0100

    Make titlebar default again

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -38,7 +38,7 @@ namespace VMsApp
             Microsoft.UI.WindowId myWndId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
             _apw = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(myWndId);
 
-            AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Collapsed;
+            AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
 
             this.FolderView.Visibility = Visibility.Collapsed;
             this.TabsGrid.Margin = new Thickness(201, 48, 0, 32);
@@ -46,15 +46,6 @@ namespace VMsApp
             if (ShowHideLibrary.IsChecked == false)
             {
                 ShowHideLibrary.IsChecked = true;
-            }
-
-            if (WindowState == WindowState.Normal)
-            {
-                MaximizeIcon.Glyph = "\uE922";
-            }
-            else
-            {
-                MaximizeIcon.Glyph = "\uE923";
             }
         }
         public static void CreateModalWindow(WindowEx parentWindow, WindowEx childWindow, bool summonWindowAutomatically = true, bool blockInput = false)
@@ -177,27 +168,6 @@ namespace VMsApp
         {
             FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
         }
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (WindowState == WindowState.Normal)
-            {
-                this.Maximize();
-                MaximizeIcon.Glyph = "\uE923";
-            }
-            else
-            {
-                this.Restore();
-                MaximizeIcon.Glyph = "\uE922";
-            }
-        }
-        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Minimize();
-        }
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
@@ -216,75 +186,6 @@ namespace VMsApp
         {
             var logWin = new Preferences();
             CreateModalWindow(App.m_window, logWin, true, true);
-        }
-        private void AppTitleBar_PointerReleased(object sender, PointerRoutedEventArgs e)
-        {
-            //nXWindow = _apw.Position.X;
-            //nYWindow = _apw.Position.Y;
-            ((UIElement)sender).ReleasePointerCaptures();
-            bMoving = false;
-        }
-        private void AppTitleBar_PointerPressed(object sender, PointerRoutedEventArgs e)
-        {
-            var properties = e.GetCurrentPoint((UIElement)sender).Properties;
-            if (properties.IsLeftButtonPressed)
-            {
-                ((UIElement)sender).CapturePointer(e.Pointer);
-                nXWindow = _apw.Position.X;
-                nYWindow = _apw.Position.Y;
-                Windows.Graphics.PointInt32 pt;
-                GetCursorPos(out pt);
-                nX = pt.X;
-                nY = pt.Y;
-                //Console.Beep(1000, 10);
-                bMoving = true;
-            }
-            else if (properties.IsRightButtonPressed)
-            {
-
-            }
-        }
-        private void AppTitleBar_PointerMoved(object sender, PointerRoutedEventArgs e)
-        {
-            //Microsoft.UI.Input.PointerPoint pp = e.GetCurrentPoint((UIElement)sender);
-            //Point ptElement = new Point(pp.Position.X, pp.Position.Y);
-            //IEnumerable<UIElement> elementStack = VisualTreeHelper.FindElementsInHostCoordinates(ptElement, (UIElement)sender);
-            //foreach (UIElement item in elementStack)
-            //{
-            //    FrameworkElement feItem = item as FrameworkElement;
-            //    //cast to FrameworkElement, need the Name property
-            //    if (feItem != null)
-            //    {
-            //        if (feItem.Name.Equals("myButton"))
-            //        {
-            //            return;
-            //        }
-            //    }
-            //}
-
-            var properties = e.GetCurrentPoint((UIElement)sender).Properties;
-            if (properties.IsLeftButtonPressed)
-            {
-                //Console.Beep(8000, 10);
-
-                Windows.Graphics.PointInt32 pt;
-                GetCursorPos(out pt);
-
-                //if (((UIElement)sender).GetType() == typeof(StackPanel))
-                if (bMoving)
-                    _apw.Move(new Windows.Graphics.PointInt32(nXWindow + (pt.X - nX), nYWindow + (pt.Y - nY)));
-
-                //Microsoft.UI.Input.PointerPoint pp = e.GetCurrentPoint((UIElement)sender);
-                //pt.X -= (int)pp.Position.X;
-                //pt.Y -= (int)pp.Position.Y;
-                //pt.X -=8;
-                //pt.Y -= 31;
-                //Windows.Graphics.PointInt32 pt = new Windows.Graphics.PointInt32((int)pp.Position.X, (int)pp.Position.Y);               
-                //IntPtr pPoint = Marshal.AllocHGlobal(Marshal.SizeOf(pt));
-                //Marshal.StructureToPtr(pt, pPoint, false);
-                //PostMessage(hWnd, WM_NCLBUTTONDOWN, HTCAPTION, pPoint);
-                e.Handled = true;
-            }
         }
     }
 }
