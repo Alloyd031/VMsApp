b1772e72dd21846014ad5561618fc9e7a0fdbad4
Alloyd <kirillpravikov@outlook.com>
2024-06-04 12:26:06 +0300

    some changes to test too

--- Preferences.xaml.cs
+++ Preferences.xaml.cs
@@ -22,97 +22,12 @@ namespace VMsApp
 {
     public sealed partial class Preferences : WindowEx
     {
-        [DllImport("User32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        public static extern bool GetCursorPos(out Windows.Graphics.PointInt32 lpPoint);
-        IntPtr hWnd = IntPtr.Zero;
-        IntPtr hWndChild = IntPtr.Zero;
-        private Microsoft.UI.Windowing.AppWindow _apw;
-        private bool bMoving = false;
-        private int nX = 0, nY = 0, nXWindow = 0, nYWindow = 0;
         public Preferences()
         {
             this.InitializeComponent(); ExtendsContentIntoTitleBar = true;
-            AppWindow.Resize(new SizeInt32(910, 790));
+            AppWindow.Resize(new SizeInt32(900, 700));
             this.CenterOnScreen();
-
-            hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
-            Microsoft.UI.WindowId myWndId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
-            _apw = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(myWndId);
-
-            AppWindow.TitleBar.PreferredHeightOption = TitleBarHeightOption.Collapsed;
-        }
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-        private void PreferencesTitleBar_PointerReleased(object sender, PointerRoutedEventArgs e)
-        {
-            //nXWindow = _apw.Position.X;
-            //nYWindow = _apw.Position.Y;
-            ((UIElement)sender).ReleasePointerCaptures();
-            bMoving = false;
-        }
-        private void PreferencesTitleBar_PointerPressed(object sender, PointerRoutedEventArgs e)
-        {
-            var properties = e.GetCurrentPoint((UIElement)sender).Properties;
-            if (properties.IsLeftButtonPressed)
-            {
-                ((UIElement)sender).CapturePointer(e.Pointer);
-                nXWindow = _apw.Position.X;
-                nYWindow = _apw.Position.Y;
-                Windows.Graphics.PointInt32 pt;
-                GetCursorPos(out pt);
-                nX = pt.X;
-                nY = pt.Y;
-                //Console.Beep(1000, 10);
-                bMoving = true;
-            }
-            else if (properties.IsRightButtonPressed)
-            {
-
-            }
-        }
-        private void PreferencesTitleBar_PointerMoved(object sender, PointerRoutedEventArgs e)
-        {
-            //Microsoft.UI.Input.PointerPoint pp = e.GetCurrentPoint((UIElement)sender);
-            //Point ptElement = new Point(pp.Position.X, pp.Position.Y);
-            //IEnumerable<UIElement> elementStack = VisualTreeHelper.FindElementsInHostCoordinates(ptElement, (UIElement)sender);
-            //foreach (UIElement item in elementStack)
-            //{
-            //    FrameworkElement feItem = item as FrameworkElement;
-            //    //cast to FrameworkElement, need the Name property
-            //    if (feItem != null)
-            //    {
-            //        if (feItem.Name.Equals("myButton"))
-            //        {
-            //            return;
-            //        }
-            //    }
-            //}
-
-            var properties = e.GetCurrentPoint((UIElement)sender).Properties;
-            if (properties.IsLeftButtonPressed)
-            {
-                //Console.Beep(8000, 10);
-
-                Windows.Graphics.PointInt32 pt;
-                GetCursorPos(out pt);
-
-                //if (((UIElement)sender).GetType() == typeof(StackPanel))
-                if (bMoving)
-                    _apw.Move(new Windows.Graphics.PointInt32(nXWindow + (pt.X - nX), nYWindow + (pt.Y - nY)));
-
-                //Microsoft.UI.Input.PointerPoint pp = e.GetCurrentPoint((UIElement)sender);
-                //pt.X -= (int)pp.Position.X;
-                //pt.Y -= (int)pp.Position.Y;
-                //pt.X -=8;
-                //pt.Y -= 31;
-                //Windows.Graphics.PointInt32 pt = new Windows.Graphics.PointInt32((int)pp.Position.X, (int)pp.Position.Y);               
-                //IntPtr pPoint = Marshal.AllocHGlobal(Marshal.SizeOf(pt));
-                //Marshal.StructureToPtr(pt, pPoint, false);
-                //PostMessage(hWnd, WM_NCLBUTTONDOWN, HTCAPTION, pPoint);
-                e.Handled = true;
-            }
+            SetTitleBar(PreferencesWindowTitleBar);
         }
         private void NavigationView_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
         {
